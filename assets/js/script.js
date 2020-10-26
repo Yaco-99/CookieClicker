b3257847dec679fad7a5a19f9d169dfab82b2b38
Kultigin Karagenc <karagenc.kultigin@gmail.com>
2020-10-26 11:59:05 +0100

    kultik function score display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,52 @@
 const scoreTarget = document.getElementById("score");
 let score = 0,
-  multiplierPrice = 200,
-  multiplier = 1,
-  timing = 0;
+    multiplierPrice = 200,
+    multiplier = 1,
+    timing = 0;
 
 scoreTarget.addEventListener("click", () => {
-  score += 1 * multiplier;
-  scoreTarget.innerHTML = score;
+    score += 1 * multiplier;
+    scoreTarget.innerHTML = score;
+    displayNone();
 });
 
 document.getElementById("multiplierShop").addEventListener("click", () => {
-  multiplierBuy();
+    multiplierBuy();
 });
 document.getElementById("hero").addEventListener("click", () => {
-  newHero();
+    newHero();
 });
 
 function multiplierBuy() {
-  if (score > multiplierPrice) {
-    multiplier++;
-    score = score - multiplierPrice;
-    multiplierPrice = multiplierPrice * 2;
-    document.getElementById("multiplier").innerHTML = multiplier;
-    document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
-    document.getElementById("cost").innerHTML = multiplierPrice;
-  } else {
-    alert("You don't have enough cookie !");
-  }
+    if (score > multiplierPrice) {
+        multiplier++;
+        score = score - multiplierPrice;
+        multiplierPrice = multiplierPrice * 2;
+        document.getElementById("multiplier").innerHTML = multiplier;
+        document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
+        document.getElementById("cost").innerHTML = multiplierPrice;
+    } else {
+        alert("You don't have enough cookie !");
+    }
 }
 function newHero() {
-  if (score > heroPrice) {
-    timing = 1000 - hero;
-    score = score - multiplierPrice;
-    multiplierPrice = multiplierPrice * 2;
-    document.getElementById("multiplier").innerHTML = multiplier;
-    document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
-    document.getElementById("cost").innerHTML = multiplierPrice;
-  } else {
-    alert("You don't have enough cookie !");
-  }
+    if (score > heroPrice) {
+        timing = 1000 - hero;
+        score = score - multiplierPrice;
+        multiplierPrice = multiplierPrice * 2;
+        document.getElementById("multiplier").innerHTML = multiplier;
+        document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
+        document.getElementById("cost").innerHTML = multiplierPrice;
+    } else {
+        alert("You don't have enough cookie !");
+    }
+}
+
+function displayNone() {
+    let display = document.querySelectorAll(".display");
+    if (cost < score) {
+        display.classList.add('show');
+    } else if (cost > score) {
+        display.classList.remove('show');
+    }
 }
