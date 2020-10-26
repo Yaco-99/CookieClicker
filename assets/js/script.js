96b0b816963c7c8f2b928a4516baf7e6fd367403
Yaco-99 <coopman.yannick@gmail.com>
2020-10-26 10:36:18 +0100

    pull

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let score = 0,
+  multiplierPrice = 200,
+  multiplier = 1,
+  timing = 0;
+
+document.getElementById("multiplierShop").addEventListener("click", () => {
+  multiplierBuy();
+});
+document.getElementById("hero").addEventListener("click", () => {
+  newHero();
+});
+
+function multiplierBuy() {
+  if (score > multiplierPrice) {
+    multiplier++;
+    score = score - multiplierPrice;
+    multiplierPrice = multiplierPrice * 2;
+    document.getElementById("multiplier").innerHTML = multiplier;
+    document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
+    document.getElementById("cost").innerHTML = multiplierPrice;
+  } else {
+    alert("You don't have enough cookie !");
+  }
+}
+function newHero() {
+  if (score > heroPrice) {
+    timing = 1000 - hero;
+    score = score - multiplierPrice;
+    multiplierPrice = multiplierPrice * 2;
+    document.getElementById("multiplier").innerHTML = multiplier;
+    document.getElementById("nextMultiplier").innerHTML = `X${multiplier}`;
+    document.getElementById("cost").innerHTML = multiplierPrice;
+  } else {
+    alert("You don't have enough cookie !");
+  }
+}
